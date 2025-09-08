2cec82a75ae9cefc4b0315b9be61a7c6bd27fe2e
Jeannette Garea Courtois <jeannette.garea.c@gmail.com>
2025-09-08 23:22:33 +0200

    a multiplicative group

--- group_3.py
+++ group_3.py
@@ -26,16 +26,39 @@ def indicatrice_euler(n:int):
     else: 
         for i in range(1, n+1):
             if pgcd(n, i) == 1:
-                print(i)
                 primes_with.append(i)
-    return len(primes_with)
+    return len(primes_with), primes_with
+
+#multiplicative group 
+
+class Z_n_N_x:
+    def __init__(self, n):
+        self._n = n 
+        self._classes_equivalences = []
+        
+    @property 
+    def n(self):
+        return self._n
+    
+    @property
+    def classes_equivalences(self):
+        return self._classes_equivalences
+    
+    @classes_equivalences.setter
+    def classes_equivalences(self, value):
+        self._classes_equivalences = [i for i in range(1, self._n) if pgcd(self._n, i) == 1]
+
+    
+    
                 
     
 
 
 def main():
     print("Chapter 3 of group theory")
-    print(indicatrice_euler(6))
+    Z_3_Z = Z_n_N_x(3)
+    Z_3_Z.classes_equivalences = None #the calculation is done
+    print(Z_3_Z._classes_equivalences)
 
 
 if __name__ == "__main__":
